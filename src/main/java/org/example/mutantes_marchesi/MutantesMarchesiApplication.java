a3ec4e34acc62a9464945e8d9be396e4b4d742ef
Mateomar <marchesimateo967@gmail.com>
2025-11-24 16:07:54 -0300

    Initial commit: Estructura base del proyecto

---
+++ src/main/java/org/example/mutantes_marchesi/MutantesMarchesiApplication.java
@@ -0,0 +1,13 @@
+package org.example.mutantes_marchesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantesMarchesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantesMarchesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mutantes_marchesi/MutantesMarchesiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mutantes_marchesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantesMarchesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
